542189b1c6f9c2d44b2f3ac72b2117031a72b6d8
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-10 19:58:12 +0100

    Fixed routes in use routes

---
+++ routes/subscription.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get("/", (req, res) => res.send({ title: "GET all subscriptions" }));
+
+subscriptionRouter.get("/:id", (req, res) => res.send({ title: "GET  subscription details" }));
+
+subscriptionRouter.post("/:id", (req, res) => res.send({ title: "CREATE a subscription" }));
+
+subscriptionRouter.put("/", (req, res) => res.send({ title: "UPDATE a subscription" }));
+
+subscriptionRouter.delete("/", (req, res) => res.send({ title: "DELETE a subscriptions" }));
+
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send({ title: "GET all user subscriptions" }),
+);
+
+subscriptionRouter.put("/user/:id", (req, res) => res.send({ title: "CANCEL all subscriptions" }));
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ title: "GET upcoming renewals" }),
+);
+
+export default subscriptionRouter;
