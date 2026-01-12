a4873306f80a761fd968a2d9c798938676706e8a
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-12 18:55:11 +0100

    Created signUp controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
 const signUp = async (req, res, next) => {
   const session = await mongoose.startSession();
@@ -20,8 +22,21 @@ const signUp = async (req, res, next) => {
 
     //Hash the password
     const salt = await bcrypt.genSalt(10);
-    6;
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await user.create([{ name, email, password: hashPassword }, { session }]);
+    const token = jwt.sign({ userId: newUser[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
     await session.commitTransaction();
+    session.endSession();
+
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "User created successfully",
+        data: { token, user: newUser[0] },
+      });
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
