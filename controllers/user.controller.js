fdf2d4bae81e6db4ca264bb07a4ae925ce03e196
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-13 21:25:35 +0100

    Fixed user.controller requests

---
+++ controllers/user.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getUser = async (req, res, next) => {
+  try {
+    const users = await User.find(req.params.id).select("-password");
+    res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getUsers, getUser };
