05fb5bd43c5aa4eeabae4e397a7281f17563d1d0
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-14 19:23:57 +0100

    Added subsciption creating controller

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
+import authorize from "../middleware/auth.middleware.js";
+import { createSubscription } from "../controllers/subscription.controller.js";
+
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", (req, res) => res.send({ title: "GET all subscriptions" }));
 
 subscriptionRouter.get("/:id", (req, res) => res.send({ title: "GET  subscription details" }));
 
-subscriptionRouter.post("/:id", (req, res) => res.send({ title: "CREATE a subscription" }));
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/", (req, res) => res.send({ title: "UPDATE a subscription" }));
 
