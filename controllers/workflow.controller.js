9fffdc790e896f94f7072087d19a9ff100a6beed
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-15 12:58:22 +0100

    Workflow added

---
+++ controllers/workflow.controller.js
@@ -0,0 +1,44 @@
+import { serve } from "@upstash/workflow/express";
+import Subscription from "../models/subscription.model.js";
+
+import dayjs from "dayjs";
+
+const REMINDERS = [7, 5, 2, 1];
+
+export const sendReminders = serve(async context => {
+  const { subscriptionId } = context.requestPayload;
+  const subscription = await fetchSubscription(context, subscriptionId);
+
+  if (!subscription || subscription.status !== "active") return;
+
+  const renewalDate = dayjs(subscription.renewalDate);
+
+  if (renewalDate.isBefore(dayjs())) {
+    console.log(`Renewal date has passed ${subscriptionId}.Stopping workflow.`);
+    return;
+  }
+  for (daysBefore of REMINDERS) {
+    const reminderDate = renewalDate.subtract(daysBefore, "day");
+    if (reminderDate.isAfter(dayjs())) {
+      await sleepUntilReminder(context, `Reminder ${daysBefore} daysBefore`, reminderDate);
+    }
+    await triggerReminder(context, `Reminder ${daysBefore} daysBefore`);
+  }
+});
+
+const fetchSubscription = async (context, subscriptionId) => {
+  return await context.run("get subscription", () => {
+    return Subscription.findById(subscriptionId).populate("user", "name email");
+  });
+};
+
+const sleepUntilReminder = async (context, label, date) => {
+  console.log(`Sleeping until ${label} reminder at ${date}`);
+  await context.sleepUntil(label, date.toDate());
+};
+
+const triggerReminder = async (context, label) => {
+  return await context.run(label, () => {
+    console.log(`Triggering ${label} reminder`);
+  });
+};
---
+++ routes/workflow.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const workflowRouter = new Router();
+
+workflowRouter.get("/", (req, res) => {});
+
+export default workflowRouter;
